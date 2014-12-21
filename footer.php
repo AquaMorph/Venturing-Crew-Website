2845dfb992aab3c65a809023b76776cd76e7c9e1
Christian Colglazier <aquamorphcomputing@gmail.com>
2014-12-21 12:54:02 -0500

    Initial set up

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+    <div id="content">
+        <p>Copyright &copy; <?php echo date('Y'); ?> <a href="http://venturecrew208.org/">Venturing Crew 208</a></p>
+    </div>
+</div>
---
+++ menu.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <div id="menu">
+        <ul>
+            <a href="index.php"><img src="webmenu.png"></a>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+</div>
