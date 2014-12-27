f79978767a1d9fe83d0943a6e8e16c81c1d9f0da
Christian Colglazier <aquamorphcomputing@gmail.com>
2014-12-27 14:06:41 -0500

    Added social media icons

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <div id="content">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <a href="http://venturecrew208.org/">Venturing Crew 208</a></p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <a href="http://venturecrew208.org/">Venturing Crew 208</a><a href="http://instagram.com/crew208/" target="_blank" class="social">x</a></p>
     </div>
 </div>
