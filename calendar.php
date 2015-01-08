1fb519717e38a9676f40b789e2e2145ec68566d9
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-01-08 17:10:40 -0500

    Fixed problem with iframes on iPhones

--- calendar.php
+++ calendar.php
@@ -1,3 +1,3 @@
 <?php include 'php/top.php'; top("Calendar","img/IMG_0935.JPG") ?>
-<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=vcrew208%40gmail.com&amp;color=%23F5F5F5&amp;ctz=America%2FNew_York" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=vcrew208%40gmail.com&amp;color=%23F5F5F5&amp;ctz=America%2FNew_York" style=" border-width:0 " frameborder="0" scrolling="no"></iframe>
 <?php include 'php/bottom.php'; ?>
--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <div id="content">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <a href="http://venturecrew208.org/">Venturing Crew 208</a><a href="http://instagram.com/crew208/" target="_blank" class="social">x</a></p>
+        <p>Copyright &copy; <?php if( ! ini_get('date.timezone') )date_default_timezone_set('EST'); echo date('Y'); ?> <a href="http://venturecrew208.org/">Venturing Crew 208</a><a href="http://instagram.com/crew208/" target="_blank" class="social">x</a></p>
     </div>
 </div>
